47dcb9a2b14d99fc1857cca7670bb1328f9b5b12
kenjift <kenjift@gmail.com>
2025-05-20 08:50:22 +0900

    edit playwright test

--- e2e/github.spec.ts
+++ e2e/github.spec.ts
@@ -1,15 +1,13 @@
 import { test, expect } from '@playwright/test';
 import cssescape from 'css.escape'
 
-test('test github', async ({ page }) => {
-  const baseURL = 'http://localhost:4210';
+test('test github direct', async ({ page }) => {
   await page.goto('/');
   await page.getByRole('button', { name: 'SET' }).click();
   await page.getByRole('button', { name: 'SETTING' }).click();
   await page.getByPlaceholder('https://www.example.com/api').fill('https://api.github.com');
   await page.getByRole('button', { name: 'SAVE' }).click();
   const sel = cssescape('*page=operation&path=/emojis&method=get');
-  console.log(sel)
   await page.locator(`a[href$="${sel}"]`).click();
   await page.getByRole('button', { name: 'EXECUTE' }).click();
   await expect(page.getByRole('button', { name: 'response' })).toBeVisible();
