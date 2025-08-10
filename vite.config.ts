dd187a4302581a9cecdf64017d557df3c94aee7e
funkjk <kenjift@gmail.com>
2025-08-10 14:40:21 +0900

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -3,83 +3,9 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-	plugins: [sveltekit(), nodePolyfills({
-		include: ['util', 'os', 'stream', 'crypto', 'buffer', 'process', 'events', 'assert', 'querystring', 'url', 'http', 'https', 'zlib', 'tty', 'readline', 'vm', 'timers', 'domain', 'constants', 'punycode', 'string_decoder', 'cluster']
-	})],
-	ssr: {
-		noExternal: ['@modelcontextprotocol/sdk']
-	},
-	build: {
-		rollupOptions: {
-			external: [
-				'fs/promises',
-				'fs',
-				'path',
-				'os',
-				'child_process',
-				'crypto',
-				'stream',
-				'util',
-				'events',
-				'assert',
-				'buffer',
-				'querystring',
-				'url',
-				'http',
-				'https',
-				'zlib',
-				'tty',
-				'readline',
-				'vm',
-				'perf_hooks',
-				'async_hooks',
-				'timers',
-				'domain',
-				'constants',
-				'punycode',
-				'string_decoder',
-				'cluster',
-				'worker_threads',
-				'v8',
-				'process'
-			]
-		}
-	},
-	optimizeDeps: {
-		exclude: ['@modelcontextprotocol/sdk']
-	},
+	plugins: [sveltekit(), nodePolyfills()],
 	define: {
 		'process.env.NODE_ENV': '"production"',
 		'global': 'globalThis'
-	},
-	resolve: {
-		alias: {
-			'util': 'vite-plugin-node-polyfills/polyfills/util',
-			'os': 'vite-plugin-node-polyfills/polyfills/os',
-			'stream': 'vite-plugin-node-polyfills/polyfills/stream',
-			'crypto': 'vite-plugin-node-polyfills/polyfills/crypto',
-			'buffer': 'vite-plugin-node-polyfills/polyfills/buffer',
-			'process': 'vite-plugin-node-polyfills/polyfills/process',
-			'events': 'vite-plugin-node-polyfills/polyfills/events',
-			'assert': 'vite-plugin-node-polyfills/polyfills/assert',
-			'querystring': 'vite-plugin-node-polyfills/polyfills/qs',
-			'url': 'vite-plugin-node-polyfills/polyfills/url',
-			'http': 'vite-plugin-node-polyfills/polyfills/http',
-			'https': 'vite-plugin-node-polyfills/polyfills/https',
-			'zlib': 'vite-plugin-node-polyfills/polyfills/zlib',
-			'tty': 'vite-plugin-node-polyfills/polyfills/tty',
-			'readline': 'vite-plugin-node-polyfills/polyfills/readline',
-			'vm': 'vite-plugin-node-polyfills/polyfills/vm',
-			'perf_hooks': 'vite-plugin-node-polyfills/polyfills/perf_hooks',
-			'async_hooks': 'vite-plugin-node-polyfills/polyfills/async_hooks',
-			'timers': 'vite-plugin-node-polyfills/polyfills/timers',
-			'domain': 'vite-plugin-node-polyfills/polyfills/domain',
-			'constants': 'vite-plugin-node-polyfills/polyfills/constants',
-			'punycode': 'vite-plugin-node-polyfills/polyfills/punycode',
-			'string_decoder': 'vite-plugin-node-polyfills/polyfills/string_decoder',
-			'cluster': 'vite-plugin-node-polyfills/polyfills/cluster',
-			'worker_threads': 'vite-plugin-node-polyfills/polyfills/worker_threads',
-			'v8': 'vite-plugin-node-polyfills/polyfills/v8'
-		}
 	}
 });
