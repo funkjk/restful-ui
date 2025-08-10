246616ab619139b63098e4ed1360ce83fef982b4
funkjk <kenjift@gmail.com>
2025-08-10 14:28:11 +0900

    upd logger

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,33 +1,45 @@
-import winston from "winston";
-import BrowserConsole from "winston-transport-browserconsole";
+// import winston from "winston";
+// import BrowserConsole from "winston-transport-browserconsole";
 
-const { combine, timestamp, prettyPrint, colorize, errors,  } = winston.format;
+// const { combine, timestamp, prettyPrint, colorize, errors,  } = winston.format;
 
 export const isBrowser = typeof window !== 'undefined';
 
 
 export const transports = isBrowser ? [
-    new BrowserConsole(
-        {
-            format: combine(
-                errors({ stack: true }), // <-- use errors format
-                colorize(),
-                timestamp(),
-                prettyPrint()
-            )
-        }
-    )
+    // new BrowserConsole(
+    //     {
+    //         format: combine(
+    //             errors({ stack: true }), // <-- use errors format
+    //             colorize(),
+    //             timestamp(),
+    //             prettyPrint()
+    //         )
+    //     }
+    // )
 ] : [
     // new winston.transports.Console(),
-    new winston.transports.File({ filename: '/Users/kenji.funaki/projects/prv/product/restful-ui/mcp-configs/mcp-logger.log' })
+    // new winston.transports.File({ filename: '/Users/kenji.funaki/projects/prv/product/restful-ui/mcp-configs/mcp-logger.log' })
 ]
 
 export function createLogger(category?: string) {
-    return winston.createLogger({
-        level: 'info',
-        format: winston.format.json(),
-        transports: transports
-    })
+    return {
+        info: (message: any) => {
+            console.log(message);
+        },
+        error: (message: any) => {
+            console.error(message);
+        },
+        warn: (message: any) => {
+            console.warn(message);
+        },
+        debug: (message: any) => {
+            console.debug(message);
+        },
+        trace: (message: any) => {
+            console.trace(message);
+        }
+    }
 }
 
 export const defaultLogger = createLogger()
