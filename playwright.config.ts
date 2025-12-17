8cbc13ae9724c8019c2c38d2b4d3409a5f4d60d8
funkjk <kenjift@gmail.com>
2025-12-17 15:45:38 +0900

    edit e2e config

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
-  fullyParallel: true,
+  fullyParallel: false,
+  globalTimeout: 100000,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
